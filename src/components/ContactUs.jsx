94a97cbc8103254c5a0711e591d864a8a80019be
Alan <alan4tomaz8@gmail.com>
2024-01-19 22:33:44 -0300

    Commit 4 - 01/19/2024

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './ContactUs.css';
 
-function ContactUs() {
+function ContactUs({ bgColor = "#cbcbcb" }) {
     return (
-        <div>
-            Contact Us
+        <div className='contact-us' style={{ backgroundColor: bgColor }}>
+            <div className="contact-us__info" >
+                <h2>Contact Us</h2>
+                <p>Fill out the form to contact us, or see our other contact methods. We'd love to help you!</p>
+                <Link to="/contact" className='button--black contact-us__button-1'>Other Contacts</Link>
+            </div>
+            <form className="contact-us__form">
+                <input type="text" placeholder='Name' />
+                <input type="text" placeholder='E-mail' />
+                <textarea cols="30" rows="10" placeholder='Message'></textarea>
+                <Link className='button--blue contact-us__button-2'>Send</Link>
+            </form>
         </div>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import './Footer.css';
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import Logo from '../images/logo.png';
 
 function Footer() {
     return (
-        <div>
-            Footer
+        <div className='footer'>
+            <div className="footer__upside">
+                <div className="footer__info">
+                    <div className="footer__logo">
+                        <img src={Logo} alt="Logo" />
+                        <h2 className="footer__title">NightDrive</h2>
+                    </div>
+                    <p className='footer__slogan'>The best way to buy and sell cars</p>
+                </div>
+                <div className="footer__links-box">
+                    <div className="footer__links">
+                        <span>WEEBLY THEMES</span>
+                        <span>PRE-SALE FAOS</span>
+                        <span>SUBMIT A TICKET</span>
+                    </div>
+                    <div className="footer__links">
+                        <span>SERVICES</span>
+                        <span>THEME TWEAK</span>
+                    </div>
+                    <div className="footer__links">
+                        <span>SHOWCASE</span>
+                        <span>WIDGET TICKET</span>
+                        <span>SUPPORT</span>
+                    </div>
+                    <div className="footer__links">
+                        <span>HOME</span>
+                        <span>ABOUT</span>
+                        <span>CONTACT</span>
+                        <span>PRODUCTS</span>
+                    </div>
+                </div>
+            </div>
+            <hr className="footer-row" />
+            <div className="footer__downside">
+                <div className="footer__social-medias">
+                    <div className="footer__twitter">
+                        <FaTwitter />
+                    </div>
+                    <div className="footer__instagram">
+                        <FaInstagram />
+                    </div>
+                    <div className="footer__facebook">
+                        <FaFacebook />
+                    </div>
+                    <div className="footer__email">
+                        <MdEmail />
+                    </div>
+                </div>
+                <p className='footer__copy'>&copy; Copyright. All rights reserved</p>
+            </div>
         </div>
     )
 }
